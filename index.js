d391bcd1c959230303af1da882676fc810c71fe9
Hivik43265 <fidotov1997@gmail.com>
2024-06-29 16:12:03 +0300

    f

--- index.js
+++ index.js
@@ -1,42 +1,25 @@
-/* Устанавливаем стартовый индекс слайда по умолчанию: */
 let slideIndex = 1;
-let i = 1;
 let x = 0;
-/* Вызываем функцию, которая реализована ниже: */
-showSlides(slideIndex);
+let slides = document.getElementsByClassName("slide");
 
-/* Увеличиваем индекс на 1 — показываем следующий слайд: */
+showSlides(x);
+
 function nextSlide() {
-    showSlides(slideIndex += 1);
+    if(x != (-(slides.length-1)*1155)){
+        showSlides(x += -1155)
+    }
 }
 
-/* Уменьшаем индекс на 1 — показываем предыдущий слайд: */
 function previousSlide() {
-    showSlides(slideIndex -= 1);  
+    if(x !=0){
+         showSlides(x += 1155)
+    }
 }
 
-/* Устанавливаем текущий слайд: */
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-/* Функция перелистывания: */
-function showSlides(n) {
-    /* Обращаемся к элементам с названием класса "item", то есть к картинкам: */
-    let slides = document.getElementsByClassName("slidebar");;
-    
-    /* Проверяем количество слайдов: */
-    if (n > slides.length) {
-      slideIndex = 1
-    }
-    if (n < 1) {
-        slideIndex = slides.length
-    }
-  
-    /* Проходим по каждому слайду в цикле for: */
+function showSlides(x) {
     for (let slide of slides) {
-        slide.style.transform = "translateX(-1000px) scaleY(0.7) scaleX(0.7)";
+            slide.style.transform = `translateX(${x}px)`;
     }
-    /* Делаем элемент блочным: */
-    slides[slideIndex - 1].style.transform = "translateX(0px)";    
 }
+
+
