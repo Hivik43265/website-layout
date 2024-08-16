1b2037b30e1d119779e421961892497ab5108dc6
Hivik43265 <fidotov1997@gmail.com>
2024-08-16 19:18:37 +0300

    fix & new_blocks

--- js/index.js
+++ js/index.js
@@ -21,6 +21,7 @@ const popup_body = document.querySelector(".popup-body");
 online_lawyers.addEventListener("click", function showPopup(){
 
     popup.style.left = `${((w_width - popup_width)/2)}px`;
+
     popup.style.top = `${((w_height - popup_height)/2) + scrollY}px`;
     popup_background.style.top = `${scrollY}px`;
 
@@ -28,30 +29,69 @@ online_lawyers.addEventListener("click", function showPopup(){
 
         popup_background.style.display ="block";
         popup.style.display = "block"; 
+
         document.body.style.overflow = 'hidden';
         document.body.style.paddingRight = `${scrollbarWidth}px`;
     }
 });
 
 popup_close.addEventListener("click", function hidePopup(){
+
         popup_body.style.animationName ="popup-inner-transformate-reverse";
         popup.style.animationName ="popup-transformate-reverse";
        
         setTimeout(function(){
+
             document.body.style.overflow =""; 
             document.body.style.paddingRight = ""
+
             popup_background.style.display ="none";
             popup.style.display = "none";  
+
             popup.style.animationName ="popup-transformate";
             popup_body.style.animationName ="popup-inner-transformate";
+
         }, 700);
 });
 
 
 const input = document.querySelectorAll("#popup-input");
 
+const feedback_button = document.querySelectorAll(".feedback-button");
+
 popup_close.addEventListener("click", function inputClear(){
+
     for(let i=0;i<input.length;i++){
         input[i].value = "";
+    }
+});
+
+const seo_text = document.querySelector(".seo-text-text");
+
+const button_show_text = document.querySelector(".button-show-text");
+
+const show_text = document.querySelector(".show-text");
+
+const text_seo = document.querySelectorAll(".text-from-seo");
+
+
+button_show_text.addEventListener("click", function showText(){
+
+    let k = 0;
+
+    if (getComputedStyle(seo_text).height == "165px"){
+
+        for(let i = 0; i < text_seo.length; i++){
+            k += text_seo[i].clientHeight;
+        }
+        
+        seo_text.style.height =`${k+(16*text_seo.length)}px`;
+
+        show_text.innerHTML = "Скрыть весь текст";
+
+    }else{
+        seo_text.style.height = "";
+        
+        show_text.innerHTML = "Показать весь текст";
     }
 });
